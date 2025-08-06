6d081ca473dbbc76d30bb6971a71b695a0541aeb
ganggyunggyu <qwzx16@naver.com>
2025-08-06 18:35:36 +0900

    chore: add lib, https setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
-export default defineConfig({
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+// });
+
+import fs from 'fs';
+import vue from '@vitejs/plugin-vue';
+
+const cert = fs.readFileSync('localhost.pem');
+const key = fs.readFileSync('localhost-key.pem');
+
+export default {
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+    ],
+  },
+  server: {
+    host: true,
+    https: {
+      key,
+      cert,
+    },
+  },
+};
