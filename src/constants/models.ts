391a22b454e9c4ee560af43080af065f18c77edc
ganggyunggyu <qwzx16@naver.com>
2025-08-21 15:18:19 +0900

    feat: add model gpt-4-v2

--- src/constants/models.ts
+++ src/constants/models.ts
@@ -1,10 +1,16 @@
-export type ModelService = 'gpt' | 'claude' | 'solar' | 'gemini' | 'gpt-5'
+export type ModelService =
+  | "gpt"
+  | "claude"
+  | "solar"
+  | "gemini"
+  | "gpt-5"
+  | "gpt-4-v2";
 
 export const MODEL_OPTIONS: { label: string; value: ModelService }[] = [
-  { label: 'GPT', value: 'gpt' },
-  { label: 'GPT5', value: 'gpt-5' },
-  { label: 'Gemini', value: 'gemini' },
-  { label: 'Claude', value: 'claude' },
-  { label: 'Solar', value: 'solar' }
-]
-
+  { label: "GPT", value: "gpt" },
+  { label: "GPT5", value: "gpt-5" },
+  { label: "GPT4V2", value: "gpt-4-v2" },
+  { label: "Gemini", value: "gemini" },
+  { label: "Claude", value: "claude" },
+  { label: "Solar", value: "solar" },
+];
--- src/service/chat.service.ts
+++ src/service/chat.service.ts
@@ -1,40 +1,41 @@
 import { axios } from "../app";
 
 type GenerationRequest = {
-  service: "gpt" | "claude" | 'solar' | 'gemini' | 'gpt-5'
-  keyword: string
-  ref: string
-}
+  service: "gpt" | "claude" | "solar" | "gemini" | "gpt-5" | "gpt-4-v2";
+  keyword: string;
+  ref: string;
+};
 type GetCategoryReq = {
-  keyword: string
-}
+  keyword: string;
+};
 
-const API = import.meta.env.VITE_API_URL
+const API = import.meta.env.VITE_API_URL;
 
 export const generateText = async (params: GenerationRequest) => {
- 
   try {
-    const response = await axios.post(`${API}/generate/${params.service}`, params)
-    return response.data
+    const response = await axios.post(
+      `${API}/generate/${params.service}`,
+      params
+    );
+    return response.data;
   } catch (error: any) {
-    console.error(error)
-    
-    throw error
-  }
-}
+    console.error(error);
 
-export const getCategory = async (params:GetCategoryReq) => {
+    throw error;
+  }
+};
+
+export const getCategory = async (params: GetCategoryReq) => {
   try {
-    
-    const response = await axios.get(`${API}/generate/${params.keyword}`)
-    
-    return response.data
-  } catch (error) {
-    console.error(error)
+    const response = await axios.get(`${API}/generate/${params.keyword}`);
 
-    throw error
+    return response.data;
+  } catch (error) {
+    console.error(error);
+
+    throw error;
   }
-}
+};
 
 // http://43.201.97.68
 
